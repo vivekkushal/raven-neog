3dda34afcacfea5dc35cc95caa317b0711300016
vivekkushal <vivekjss1@gmail.com>
2023-05-31 21:59:12 +0530

    feat: Add login, signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Mockman from 'mockman-js';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 
 function App() {
   return (
@@ -45,6 +47,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mockman" element={<Mockman />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
