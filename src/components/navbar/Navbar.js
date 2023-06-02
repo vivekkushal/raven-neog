d7dfac09611a6b121f26f333760384d612b8bc6f
vivekkushal <vivekjss1@gmail.com>
2023-06-03 01:32:23 +0530

    style: Add responsive navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,50 @@
 import './Navbar.css';
 
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [navbarClass, setNavbarClass] = useState('');
+
+  const toggleHandler = () => {
+    setNavbarClass((prev) => (prev === '' ? ' active' : ''));
+  };
+
   return (
     <nav className="navbar">
-      <li>Raven</li>
-      <li>
-        <Link to="/login" className="login-link">
-          Login
-        </Link>
-      </li>
-      <li>Wishlist</li>
-      <li>Cart</li>
+      <Link to="/" className="home-link">
+        Raven
+      </Link>
+      <Link to="/search" className="search-link">
+        Q
+      </Link>
+      <div className={`navbar-links${navbarClass}`}>
+        <li>
+          <Link to="/search" className="inner-search-link">
+            Q
+          </Link>
+        </li>
+        <li>
+          <Link to="/wishlist" className="wishlist-link">
+            Wish<sup></sup>
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart" className="cart-link">
+            Cart<sup></sup>
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="login-link">
+            Login
+          </Link>
+        </li>
+      </div>
+      <div onClick={toggleHandler} className="navbar-toggle">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
     </nav>
   );
 }
